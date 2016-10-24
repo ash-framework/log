f8db8d96d1faa84c2c8d10b194e9ed15abe03a2a
Richard Walker <digitalsadhu@gmail.com>
2016-10-24 15:09:22 +0200

    Add module and implementation

--- test/default.spec.js
+++ test/default.spec.js
@@ -1,7 +1,88 @@
-/* global describe, it */
-var assert = require('assert')
-describe('the module', function () {
-  it('should should be ok', function () {
-    assert.ok(true)
+/* global describe, it, afterEach */
+
+const Log = require('../src')
+const td = require('testdouble')
+const assert = require('assert')
+const NODE_ENV = process.env.NODE_ENV
+
+describe('Log', function () {
+  afterEach(() => {
+    process.env.NODE_ENV = NODE_ENV
+    td.reset()
+  })
+
+  it('fatal', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.fatal('fatal error message')
+
+    td.verify(logger.adapter.fatal('fatal error message'))
+  })
+
+  it('error', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.error('custom error message')
+
+    td.verify(logger.adapter.error('custom error message'))
+  })
+
+  it('warn', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.warn('custom warning')
+
+    td.verify(logger.adapter.warn('custom warning'))
+  })
+
+  it('info', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.info('custom info')
+
+    td.verify(logger.adapter.info('custom info'))
+  })
+
+  it('debug', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.debug('custom debug message')
+
+    td.verify(logger.adapter.debug('custom debug message'))
+  })
+
+  it('trace', function () {
+    let logger = new Log()
+    td.replace(logger, 'adapter')
+
+    logger.trace('custom trace message')
+
+    td.verify(logger.adapter.trace('custom trace message'))
+  })
+
+  it('NODE_ENV === production', function () {
+    process.env.NODE_ENV = 'production'
+    let logger = new Log()
+
+    assert.strictEqual(logger.adapter.level, 'error')
+  })
+
+  it('NODE_ENV === development', function () {
+    process.env.NODE_ENV = 'development'
+    let logger = new Log()
+
+    assert.strictEqual(logger.adapter.level, 'info')
+  })
+
+  it('NODE_ENV === debug', function () {
+    process.env.DEBUG = 'true'
+    let logger = new Log()
+
+    assert.strictEqual(logger.adapter.level, 'trace')
   })
 })
